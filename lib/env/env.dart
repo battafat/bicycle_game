b533d5eff4d7a9e7261eef3a63bd20b18d4a37e5
timothy battafarano <tim@deepsprings.edu>
2024-10-09 15:27:09 -0500

    Save local changes before upgrading Flutter

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,6 +4,6 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 final class Env {
-  @EnviedField(varName: 'MAPS_Key')
-  static const String mapsAPIKey = _Env.mapsAPIKey;
+  @EnviedField(varName: 'MAPS_Key', obfuscate: true)
+  static final String mapsAPIKey = _Env.mapsAPIKey;
 }
